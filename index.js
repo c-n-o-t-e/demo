87878d63ac722e5e3c05b0e64c4d1a84e448e2c6
c-note <dongustavo20@gmail.com>
2022-10-06 11:14:02 +0100

    u

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ const server = createServer(app);
 const { Client, GatewayIntentBits } = require("discord.js");
 const token = process.env.TOKEN;
 
+const abi = require("./abi.json");
+const ethers = require("ethers");
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -21,6 +24,22 @@ const client = new Client({
     GatewayIntentBits.GuildMembers,
   ],
 });
+
+const network = {
+  name: "goerli",
+  chainId: 5,
+  _defaultProvider: (providers) =>
+    new providers.JsonRpcProvider(
+      `https://eth-goerli.alchemyapi.io/v2/RuGxhTpkUt3S6InAt33l_3NZc4pFwXxS`
+    ),
+};
+
+const provider = ethers.getDefaultProvider(network);
+const demoContract = new ethers.Contract(
+  "0x36305dfb6f2613Be99BfA30dfE6C322738eA1479",
+  abi,
+  provider
+);
 
 client.on("ready", () => {
   console.log(`logged in as ${client.user.tag}`);
@@ -38,6 +57,11 @@ client.on("guildMemberAdd", async (member) => {
   member.guild.channels.cache.get(welcomeChannelId).send({
     content: `<@${member.id}> Welcome to the server!`,
   });
+});
+
+demoContract.on("NewMsg", async (event) => {
+  const discordChannel = client.channels.cache.get(welcomeChannelId);
+  discordChannel.send({ content: `new NewMsg event $${event}` });
 });
 
 client.login(token);
